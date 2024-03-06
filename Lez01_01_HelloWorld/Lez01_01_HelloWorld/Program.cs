358c60711f23f0eadf6c6fb4c017145e7721e0e3
Tiziano Titone <titone1996@gmail.com>
2024-03-06 09:38:03 +0100

    add

---
+++ Lez01_01_HelloWorld/Lez01_01_HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lez01_01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sono un commento 
+            /*
+             * Commento 
+             * multilinea 
+             */
+            Console.WriteLine("Hello, World1!");     // Sono un commento 
+            Console.WriteLine("Hello, World2!");
+
+            Console.ReadLine();     // La console ha bisogno di un input per andare avanti
+
+            Console.Write("Ciao "); 
+            Console.Write("Ciao ");                  //Console.Write non va a capo
+
+            Console.WriteLine("Hello, World3!");
+            Console.WriteLine("Hello, World4!");
+
+            Console.WriteLine("Ciao Pippo");       
+        }
+    }
+}
